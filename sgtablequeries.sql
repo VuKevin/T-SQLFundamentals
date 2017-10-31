349c7c86a81ad021e2050c17e7d1d2bc2381bca2
Kevin Vu <VuKevin@icloud.com>
2017-10-31 15:58:17 -0700

    Update sgtablequeries.sql

--- sgtablequeries.sql
+++ sgtablequeries.sql
@@ -1,15 +1,16 @@
 USE HPS;
 SELECT * FROM HR.Employees;
---  {orderid, custid, empid, orderdate, requireddate, shippeddate, shipperid, freight, shipname, shipaddress, shipcity, shipregion, shippostalcode, shipcountry} ∈ Sales.Orders
 
 -- #1. Write a query against the Sales.Orders table that returns orders placed in June 2007.
 -- Tables involved: TSQL2012 database and the Sales.Orders table
 
-/* All other fields are irrelevant for purposes of the problem */
 SELECT orderid, orderdate, custid, empid 
 FROM Sales.Orders
-/* WHERE clause is used to filter, YEAR(datefield) and MONTH(datefield) takes a date as an argument and returns an INT */
 WHERE YEAR(orderdate) = 2007 AND MONTH(orderdate) = 6; 
+
+/* Notes:
+	WHERE clause is used to filter, YEAR(datefield) and MONTH(datefield) takes a date as an argument and returns an INT 
+*/
 
 -- #2. Write a query against the Sales.Orders table that returns orders placed on the last day of the month
 -- Tables involved: TSQL2012 database and the Sales.Orders table
@@ -28,16 +29,25 @@ WHERE DAY(orderdate) LIKE
 			DAY(orderdate)
 	END;
 
-
-/*	empid, lastname, firstname, title, titleofcourtesy, birthdate, hiredate, address, city, region, postalcode, country, phone, mgrid ∈ Hr.Employees */ 
+/* Notes:
+	A switch statement is used to aid in filtering cases where the last day falls on the 31st, on the 30th, 
+	or special cases with February and leap years.
+*/
 
 -- #3. Write a query against the HR.Employees Table that returns employees with last name containing the letter a twice or more.
 -- Tables involved: TSQL2012 database and the HR.Employees table
 
 SELECT empid, firstname, lastname
 FROM HR.Employees
--- WHERE lastname like '%a%a';
 WHERE len(lastname) - len(replace(lastname, 'a', '')) >= 2;
+-- WHERE lastname like '%a%a';
+
+/* Notes:
+	Steps include getting the length of the last name and subtracting it with the length of the last name removing all the a's
+	If the resulting difference is greater or equal to two, then that employee's last name must contain at least 2 a's.
+	
+	Alternatively, %a%a will also match lastnames to those with at least two a's contained.
+*/
 
 -- #4. Write a query against the Sales.OrderDetails table that returns orders with total value (quantity*unit-price) greater than 10k AND sort it by total value
 -- Tables involved: TSQL2012 database and the Sales.OrderDetails table
@@ -45,8 +55,22 @@ WHERE len(lastname) - len(replace(lastname, 'a', '')) >= 2;
 SELECT orderid, SUM(qty * unitprice) as totalvalue
 FROM Sales.OrderDetails
 GROUP BY orderid
-HAVING SUM(qty * unitprice) > 10000 -- WHERE keyword can't be used with aggregate functions
+HAVING SUM(qty * unitprice) > 10000 
 ORDER BY totalvalue DESC;
+
+/* Notes:
+	HAVING is used in place of WHERE because WHERE is unable to handle aggregate functions. 
+	In this case, we took SUM(qty*unitprice).
+	
+	HAVING Syntax:
+	SELECT column_name(s)
+	FROM table_name
+	WHERE condition -- optional
+	GROUP BY column_names(s)
+	HAVING condition
+	ORDER BY column_names(s);
+	
+*/
 
 -- #5. Write a query against the Sales.Orders table that returns the three shipped-to countries with the highest average freight in 2007.
 -- Tables involved: TSQL2012 database and the Sales.Orders table
@@ -57,14 +81,22 @@ WHERE YEAR(orderdate) = 2007
 GROUP BY shipcountry
 ORDER BY avgfreight DESC;
 
+/* Notes:
+	SELECT TOP num clause specifies the number of records to return that are of interest.
+	AVG() returns the average value of a numeric column
+*/
 
 -- #6. Write a query against the Sales.Orders table that calculates row numbers for orders based on order date ordering (using the order ID as the tiebreaker) for each customer separately.
 -- Tables involved: TSQL2012 database and the Sales.Orders table
 
--- Partition required because "each customer is calculated separately"
 SELECT custid, orderdate, orderid, ROW_NUMBER() OVER(PARTITION BY custid ORDER BY orderdate, orderid) AS rownum
 FROM Sales.Orders
-ORDER BY custid, rownum -- order by precedence
+ORDER BY custid, rownum 
+
+/* Notes:
+	ROW_NUMBER() OVER(PARTITION BY column_name ORDER BY column_name, column_name)
+	Note: ROW_NUMBER() and RANK() are similar except RANK provides the same numeric value for ties.
+*/
 
 -- #7. Using the HR.Employees table, figure out the SELECT statement that returns for each employee the
 -- gender based on the title of courtesy. For ‘Ms. ‘ and ‘Mrs.’ return ‘Female’; for ‘Mr. ‘ return ‘Male’; and
@@ -80,6 +112,9 @@ SELECT empid, firstname, lastname, titleofcourtesy,
 		END as gender
 FROM Hr.Employees;
 
+/* Notes:
+	It is possible, as shown here, to use a switch statement in the formation of a new field (gender in this case)
+*/
 
 -- #8. Write a query against the Sales.Customers table that returns for each customer the customer ID and
 -- region. Sort the rows in the output by region, having NULL marks sort last (after non-NULL values).
